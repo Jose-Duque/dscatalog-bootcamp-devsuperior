8089144cc95ef4b482475033249166cb786b56ca
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-21 09:41:20 -0300

    Initil, scss custom

--- backend/front-web/src/App.tsx
+++ backend/front-web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './App.scss';
 
 const App = () => {
   return (
-    <h1>App</h1>
-  ); 
+   <h1>TEste</h1>
+  );
 }
 
 export default App;
