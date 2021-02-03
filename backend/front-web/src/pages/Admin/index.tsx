e793583b05a528e0e874dea642e5b70fe9613d34
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-03 13:44:26 -0300

    Add Admin Navbar

--- backend/front-web/src/pages/Admin/index.tsx
+++ backend/front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
-const Admin = () => (
+const Admin = () => {
+ return (
   <div>
-    <h1>Admin</h1>
+    <Navbar/>
   </div>
-);
+) 
+};
 
 export default Admin;
