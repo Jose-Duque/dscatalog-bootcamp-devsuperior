0580355716c20b331df79b8f98d3d2fd84125f8b
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-22 09:25:37 -0300

    Postgres conpatibility

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -33,9 +34,9 @@ public class ProductService {
 	private CategoryRepository categoryrepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest){
+		List<Category> categories =  (categoryId == 0) ? null : Arrays.asList(categoryrepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories, name.trim(), pageRequest);
 		return list.map(x -> new ProductDTO(x));
 		
 		/* Forma mais verbosa
